3ff88b086b4bd59da6ffeead278c48e164812dca
Jassim Vailoces <jassimv88@yahoo.com>
2023-07-31 23:27:06 -0400

    using create element in adding new boxes

--- main.js
+++ main.js
@@ -40,52 +40,65 @@ const products = [
 ]
 const cartItems = [
 ]
+populateProducts()
+
 function populateProducts() {
   boxesDiv.innerHTML = ""
   for (let i = 0; i <products.length; i++) {
     // replace line 46-55. Use document.createElement to create HTML elements. Refer to do list.
-    let htmlElement = `
-    <div class="box">
-    <img src="ps5.png" alt="">
-    <h3 class="description">${products[i].product_name}</h3>
-    <h4 class="price">$${products[i].price}</h4>
-    <button data-id=${products[i].id} class="productButton">+ Add to cart</button>
-    </div>
-    `;
-    boxesDiv.innerHTML+=htmlElement
+    // let htmlElement = `
+    // <div class="box">
+    // <img src="ps5.png" alt="">
+    // <h3 class="description">${products[i].product_name}</h3>
+    // <h4 class="price">$${products[i].price}</h4>
+    // <button data-id=${products[i].id} class="productButton">+ Add to cart</button>
+    // </div>
+    // `;
+    // boxesDiv.innerHTML+=htmlElement
 
 
     const boxes = document.querySelector('.boxes');
 
     let newBox = document.createElement('div');
     newBox.classList = 'box';
-    newBox.append(boxes);
+    boxes.append(newBox);
 
     let newImg = document.createElement('img');
-    newImg.append(newBox);
+    newImg.src = 'ps5.png';
+    // or newImg.setAttribute('src', 'ps5.png');
+    newBox.append(newImg);
 
     let newH3 = document.createElement('h3');
     newH3.classList = 'description';
-    newH3 = append(newBox);
+    newH3.textContent =`${products[i].product_name}`;
+    newBox.append(newH3);
 
     let newH4 = document.createElement('h4');
-    newH4.classList = 'price';
-    newH4.append(newBox);
+    newH4.textContent = `$${products[i].price}`;
+    newBox.append(newH4);
 
     let newButton = document.createElement('button');
     newButton.classList = 'productButton';
-    newButton.append(newBox);
-
-    
-    
-    const productBtn = document.querySelector('.productButton')
-    productBtn.addEventListener('click', addToCart);
-    function addToCart() {
-      console.log(this);
-  
-    }
+    newButton.textContent = 'Add to Cart';
+    newButton.setAttribute('data-id', products[i].id);
+    newButton.addEventListener('click', addToCart);
+    newBox.append(newButton);
   }
+  // const productBtn = document.querySelector('.productButton')
+  // productBtn.addEventListener('click', addToCart);
   
+
   // event listener for add to cart. function add to cart.
 }
-populateProducts()
+
+
+function addToCart() {
+  const pId = parseInt(this.dataset.id);
+  const product = products.find(function(p){
+    return pId === p.id
+  })
+  cartItems.push(product);
+  console.log(cartItems);
+}
+
+
