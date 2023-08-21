9152b01396702e79545fe9a45cb13a39aea312db
Jassim Vailoces <jassimv88@yahoo.com>
2023-08-20 22:35:35 -0400

    changing products

--- main.js
+++ main.js
@@ -3,39 +3,39 @@ const boxesDiv = document.querySelector(".boxes")
 const products = [
   {
     id: 1,
-    product_name: "sony playstation 5",
-    price: 1000,
-    image: "",
+    product_name: "mouse",
+    price: 5,
+    image: "https://images.pexels.com/photos/301448/pexels-photo-301448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 2,
-    product_name: "sony playstation 5",
-    price: 19.99,
-    image: "",
+    product_name: "cat",
+    price: 20,
+    image: "https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 3,
-    product_name: "sony playstation 5",
-    price: 29.99,
-    image: "",
+    product_name: "dog",
+    price: 50,
+    image: "https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 4,
-    product_name: "sony playstation 5",
-    price: 39.99,
-    image: "",
+    product_name: "pig",
+    price: 100,
+    image: "https://images.pexels.com/photos/63285/pig-alp-rona-furna-sow-63285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 5,
-    product_name: "sony playstation 5",
-    price: 49.99,
-    image: "",
+    product_name: "horse",
+    price: 500,
+    image: "https://images.pexels.com/photos/1996332/pexels-photo-1996332.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 6,
-    product_name: "sony playstation 5",
-    price: 59.99,
-    image: "",
+    product_name: "elephant",
+    price: 1000,
+    image: "https://images.pexels.com/photos/3739327/pexels-photo-3739327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ]
 const cartItems = [
@@ -64,9 +64,19 @@ function populateProducts() {
     boxes.append(newBox);
 
     let newImg = document.createElement('img');
-    newImg.src = 'ps5.png';
+    newImg.src = 'https://images.pexels.com/photos/301448/pexels-photo-301448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
     // or newImg.setAttribute('src', 'ps5.png');
     newBox.append(newImg);
+
+
+    // const box1 = document.querySelector('#products#1');
+
+    // let newImg1 = document.createElement('img');
+    // newImg1.setAttribute('src', 'https://images.pexels.com/photos/301448/pexels-photo-301448.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+    // box1.append(newImg1);
+
+
+
 
     let newH3 = document.createElement('h3');
     newH3.classList = 'description';
@@ -101,6 +111,46 @@ function addToCart() {
   })
   cartItems.push(product);
   console.log(cartItems);
+  loadCartItems();
+}
+
+function loadCartItems() {
+  const cartContainer = document.querySelector('#cartItems');
+  cartContainer.innerHTML = ""
+  for(let item of cartItems) {
+    let cartItemHtml = `
+    <section class="bodyCart">
+        <div class="cartItem">
+          <img class="cartImg" src="ps5.png" alt="">
+          <div class="itemDetail">
+            <h5 >headsets</h5>
+            <p class="itemPrice">${item.price}</p>
+          </div>
+          <div class="subTotal">
+          ${item.price}
+          </div>
+          <button class="removeItemBtn">X</button>
+        </div>
+        <div class="editAmount">
+          Edit Amount
+          <button type="button" class="minus">
+            -
+          </button>
+          <span class="amount">
+          ${item.price}
+          </span>
+          <span class="inCart">
+            in cart
+          </span>
+          <button type="button" class="add">
+            +
+          </button>
+
+        </div>
+      </section>
+    `
+    cartContainer.innerHTML+=cartItemHtml
+  }
 }
 
 
